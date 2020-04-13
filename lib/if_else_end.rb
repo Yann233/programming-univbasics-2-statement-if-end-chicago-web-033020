a077d1056373843c35edf58ef02fdda4733b9f04
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 05:55:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -7,7 +7,16 @@
 #   puts "Odd!"
 # end
 
-if Time.now.to_i % 2 == 0
+# if Time.now.to_i % 2 == 0
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+
+current_time = Time.now  #Capturing the Time.now value
+current_time = current_time.to_i  #Convert it to an integer
+
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
