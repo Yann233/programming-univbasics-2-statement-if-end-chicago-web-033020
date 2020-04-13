0bed28dc8595033fea52f7e2015b3dac7b401f6c
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-13 05:56:12 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,4 +1,22 @@
-if true
+# current_time = Time.now
+# current_time = current_time.to_i
+#
+# if current_time % 2 == 0
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+
+# if Time.now.to_i % 2 == 0
+#   puts "Even!"
+# else
+#   puts "Odd!"
+# end
+
+current_time = Time.now  #Capturing the Time.now value
+current_time = current_time.to_i  #Convert it to an integer
+
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
